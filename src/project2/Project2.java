ea60b4545532f5dd69750a49e73e290dbee4f72a
jamesosborne <james.osborne112@gmail.com>
2016-09-20 14:28:51 -0400

    Initial commit to github

---
+++ src/project2/Project2.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project2;
+
+/**
+ *
+ * @author Owner
+ */
+public class Project2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Project2UI gui = new Project2UI();
+        
+        gui.setVisible(true);
+        
+        
+    }
+    
+}
